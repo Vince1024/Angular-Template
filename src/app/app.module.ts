3bf2a0cdc78568ba9d096904bde82e2f4a15edad
Vincent PAPUCHON <vincent.papuchon@disney.com>
2024-11-14 16:09:13 +0100

    Add Language Support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MainTopMenuComponent } from './main-top-menu/main-top-menu.component';
 import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatDivider } from '@angular/material/divider';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
@@ -10,7 +11,12 @@ import { MatMenuModule } from "@angular/material/menu";
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { MainTopMenuComponent } from './main-top-menu/main-top-menu.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http:HttpClient){
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -19,15 +25,23 @@ import { MainTopMenuComponent } from './main-top-menu/main-top-menu.component';
   ],
   imports: [
     AppRoutingModule,
-    MatDivider,
     BrowserModule,
+    HttpClientModule,
     MatButton,
     MatButtonModule,
+    MatDivider,
     MatIcon,
     MatIconModule,
     MatMenuModule,
     MatSlideToggleModule,
     MatToolbarModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [
     provideAnimationsAsync()

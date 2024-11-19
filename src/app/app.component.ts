6c74c13bb96ff5470533a446cde4d690d2a12549
Vincent PAPUCHON <vincent.papuchon@disney.com>
2024-11-19 10:13:41 +0100

    Theming Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,27 @@ export class AppComponent {
 
   title = 'Angular Template';
 
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  loadStyle(styleName: string) {
+    const head = this.document.getElementsByTagName('head')[0];
+
+    let themeLink = this.document.getElementById(
+      'client-theme'
+    ) as HTMLLinkElement;
+    if (themeLink) {
+      themeLink.href = `Themes/${styleName}`; //<--add assets
+    } else {
+      const style = this.document.createElement('link');
+      style.id = 'client-theme';
+      style.rel = 'stylesheet';
+      // style.type = 'text/html';
+      style.href = `Themes/${styleName}`; //<--add assets
+
+      head.appendChild(style);
+      console.log('Theme ' + styleName + ' loaded')
+    }
+  }
+
 }
+// <link id="client-theme" rel="stylesheet" type="text/html" href="Themes/purple-green.css">
